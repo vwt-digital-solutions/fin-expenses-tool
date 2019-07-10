d947c2cd74899fb7adbfc04babd7c3a1d71fced8
VWT <max.vanderweide@student.hu.nl>
2019-07-10 15:53:28 +0200

    Added 'No Input' functionality & mobile support DAT-346

--- src/app/modules/expenses/expenses.component.ts
+++ src/app/modules/expenses/expenses.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {isNumber, isString} from 'util';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
@@ -11,28 +10,28 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 export class ExpensesComponent {
   public formNote;
   public formAmount;
+  public eamount;
+  public enote;
   constructor(private http: HttpClient) {
   }
   claimForm(form: NgForm) {
     console.log(form.value);
-    const obj = JSON.parse('{ "amount":' + form.value.amount + ', "note":"' + form.value.note + ' "}');
     // tslint:disable-next-line:max-line-length
-    if (form.value.amount === null || form.value.amount === '' || form.value.note === '' || !isNumber(form.value.amount) || !isString(form.value.note)) {
-      // Change this validation with a better function
-      alert('Wrong data'); // Testing Alert
-    } else {
+    // form.value.amount === null || form.value.note === null || form.value.amount === '' || form.value.note === '' || !isNumber(form.value.amount) || !isString(form.value.note)
+    if ((typeof form.value.amount !== 'number') || (form.value.amount < 0.1)) { this.eamount = false; } else { this.eamount = true; }
+    if ((typeof form.value.note !== 'string')) { this.enote = false; } else { this.enote = true; }
+    if (this.enote && this.eamount) {
+      const timeout = 5;
+      const obj = JSON.parse('{ "amount":' + form.value.amount + ', "note":"' + form.value.note + ' "}');
       this.http.post('http://192.168.1.49:8080/employees/expenses',
         obj)
         .subscribe(
           (val) => {
-            console.log('POST call successful value returned in body',
+            console.log('POST successful and value has been added with API: ',
               val);
-          },
-          response => {
-            console.log('POST call in error', response);
-          },
-          () => {
-            console.log('The POST observable is now completed.');
+          }, response => {
+            console.log('POST failed and API has sent as response with error: ', response);
+            alert('Something went wrong. Try again later.');
           });
     }
   }
