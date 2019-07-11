42f80c27253edcd52706a8a237913d6c5979d433
P. Mutale <peter@mutale.nl>
2019-07-11 09:13:32 +0200

    Best Practices DAT-346

--- src/app/modules/expenses/expenses.component.ts
+++ src/app/modules/expenses/expenses.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { EnvService } from 'src/app/services/env.service';
 
 @Component({
   selector: 'app-expenses',
@@ -10,28 +11,26 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 export class ExpensesComponent {
   public formNote;
   public formAmount;
-  public eamount;
-  public enote;
-  constructor(private http: HttpClient) {
+  public expensesAmount;
+  public expensesNote;
+  constructor(
+    private httpClient: HttpClient,
+    private env: EnvService,
+  ) {
   }
   claimForm(form: NgForm) {
-    console.log(form.value)
-    // tslint:disable-next-line:max-line-length
-    // form.value.amount === null || form.value.note === null || form.value.amount === '' || form.value.note === '' || !isNumber(form.value.amount) || !isString(form.value.note)
-    if ((typeof form.value.amount !== 'number') || (form.value.amount < 0.1)) { this.eamount = false; } else { this.eamount = true; }
-    if ((typeof form.value.note !== 'string') || form.value.note === '') { this.enote = false; } else { this.enote = true; }
-    if (this.enote && this.eamount) {
-      const timeout = 5;
+    this.expensesAmount = !((typeof form.value.amount !== 'number') || (form.value.amount < 0.1));
+    this.expensesNote = !((typeof form.value.note !== 'string') || form.value.note === '');
+    if (this.expensesNote && this.expensesAmount) {
       const obj = JSON.parse('{ "amount":' + form.value.amount + ', "note":"' + form.value.note + ' "}');
-      this.http.post('http://192.168.1.49:8080/employees/expenses',
+      this.httpClient.post(this.env.apiUrl + '/employees/expenses',
         obj)
         .subscribe(
           (val) => {
-            console.log('POST successful and value has been added with API: ',
+            console.log('>> POST SUCCESS',
               val);
           }, response => {
-            console.log('POST failed and API has sent as response with error: ', response);
-            alert('Something went wrong. Try again later.');
+            console.error('>> POST FAILED', response.message);
           });
     }
   }
