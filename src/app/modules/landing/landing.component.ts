b3d1c5f136617376d0d53698bdbaabb2b7e4da90
Max van der Weide <max.vanderweide@student.hu.nl>
2019-08-12 10:59:26 +0200

    Safety push DAT-557

--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -1,4 +1,12 @@
 import {Component, OnInit} from '@angular/core';
+import {OAuthService} from 'angular-oauth2-oidc';
+import {HttpClient} from '@angular/common/http';
+import {EnvService} from '../../services/env.service';
+
+interface IClaimRoles {
+  oid: any;
+  roles: any;
+}
 
 @Component({
   selector: 'app-manager',
@@ -7,10 +15,37 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() {
+  private OurJaneDoeIs: string;
+  private displayPersonName;
+  private declarationData;
+
+  constructor(
+    private oauthService: OAuthService,
+    private httpClient: HttpClient,
+    private env: EnvService,
+  ) {
   }
 
   ngOnInit() {
+    const claimJaneDoe = this.oauthService.getIdentityClaims() as IClaimRoles;
+    // @ts-ignore
+    this.displayPersonName = claimJaneDoe.name.split(',');
+    this.displayPersonName = (this.displayPersonName[1] + ' ' + this.displayPersonName [0]).substring(1);
+    this.OurJaneDoeIs = claimJaneDoe.roles[0].split('.')[0];
+    console.log(claimJaneDoe);
+    this.declarationCall();
   }
 
+  declarationCall() {
+    // @ts-ignore
+    this.httpClient.get(this.env.apiUrl + '/employees/' + 'm.vanderweide' + '/expenses')
+      .subscribe(
+        val => {
+          console.log(val);
+        });
+  }
+
+  handleLinking(event) {
+    window.location.href = window.location.protocol + '//' + window.location.host + '/' + event.target.name;
+  }
 }
