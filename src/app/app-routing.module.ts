0c8723dc4ab812a4a14ef3ad86c566a628df58a0
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-09-25 14:10:10 +0200

    Elimiinate unnecessary modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {ExpensesComponent} from './modules/expenses/expenses.component';
-import {ManagerComponent} from './modules/manager/manager.component';
-import {FinanceComponent} from './modules/finance/finance.component';
-import {ControllerComponent} from './modules/controller/controller.component';
-import {LandingComponent} from './modules/landing/landing.component';
-import {Role} from './models/role.enum';
-import {AuthGuard} from './auth/auth.guard';
-import {AuthComponent} from './auth/auth.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ExpensesComponent } from './components/expenses/expenses.component';
+import { ManagerComponent } from './components/manager/manager.component';
+import { FinanceComponent } from './components/finance/finance.component';
+import { ControllerComponent } from './components/controller/controller.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { Role } from './models/role.enum';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {
@@ -21,27 +21,27 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
-      path: 'auth/:authBody',
-      component: AuthComponent,
+    path: 'auth/:authBody',
+    component: AuthComponent,
   },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   {
     path: 'expenses/process',
     component: FinanceComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.Creditor]}
+    data: { roles: [Role.Creditor] }
   },
   {
     path: 'expenses/manage',
     component: ManagerComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.Manager]}
+    data: { roles: [Role.Manager] }
   },
   {
     path: 'expenses/controller',
     component: ControllerComponent,
     canActivate: [AuthGuard],
-    data: {roles: [Role.Controller]}
+    data: { roles: [Role.Controller] }
   }
 ];
 
