7116061595ee46f120faf075d93348d9213c295e
MaxvanderWeide <max.vanderweide@student.hu.nl>
2019-11-11 13:35:26 +0100

    Fixed ng test issues DAT-1218

--- src/app/services/identity.service.spec.ts
+++ src/app/services/identity.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IdentityService } from './identity.service';
+import {OAuthLogger, OAuthService, UrlHelperService} from 'angular-oauth2-oidc';
+import {HttpClient, HttpHandler} from '@angular/common/http';
 
 describe('IdentityService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers : [OAuthService, HttpClient, HttpHandler, UrlHelperService, OAuthLogger]
+  }));
 
   it('should be created', () => {
     const service: IdentityService = TestBed.get(IdentityService);
