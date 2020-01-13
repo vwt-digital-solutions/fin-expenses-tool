1d44c125303e696f7f643ed609e348b6bd4dd247
dvanrhenen <d.vanrhenen@vwt.digital>
2020-01-13 16:16:18 +0100

    Updated styling of header to make it more sleek. DAT-1577

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,25 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { EnvService } from 'src/app/services/env.service';
 import { OAuthService } from 'angular-oauth2-oidc';
 
 interface IClaimsEmail {
   email: any;
 }
 
+declare var $: any;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-  title = 'Declaraties';
+export class HeaderComponent implements OnInit {
+  title = 'Declaratie app';
 
   constructor(
+    private env: EnvService,
     private oauthService: OAuthService
   ) {}
 
+  ngOnInit() {
+    $('body').on('click',function() {
+      if($('.navbar-collapse').hasClass('show')){
+        $('.navbar-collapse').collapse('hide');
+      }
+    });
+  }
+
   logout() {
     this.oauthService.logOut();
+  }
+
+  sendFeedback() {
+    window.location.href = `mailto:${this.feedbackEmail}?subject=Feedback%20Declaratie-app`
   }
 
   get email() {
@@ -28,5 +44,9 @@ export class HeaderComponent {
       return null;
     }
     return claims.email.toLowerCase();
+  }
+
+  get feedbackEmail() {
+    return this.env.feedbackEmail;
   }
 }
